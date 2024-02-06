0cace2e7fbb1b8821b4aa721ffba10d88787dc0f
CP Payne <charlethical1@gmail.com>
2024-02-06 11:13:38 +0200

    Initial commit after cloning follow along code. See gobank-yt-followalng

---
+++ README.md
@@ -0,0 +1,25 @@
+Part 1: https://www.youtube.com/watch?v=pwZuNmAzaH8
+Part 2: https://www.youtube.com/watch?v=gJx6gODwOwM
+Part 3: https://www.youtube.com/watch?v=mj8SW4HjZvs
+Part 4: https://www.youtube.com/watch?v=xYCVQpqQh7Q
+Part 5: https://www.youtube.com/watch?v=9VRvEzXgImM
+
+NOTE: Series incomplete, waiting for PART 6. Unlikely to be released.
+
+Continue with project on own.
+
+TODO:
+
+1. Add JWT returned from login to headers
+2. Verify token on requests that needs authorisation
+3. When making transfers, retrieve fromAccount from the JWT
+4. Clean Up Code
+
+ADDITIONAL FUNCTIONALITY:
+
+- Cleanup and improve database (normalise)
+
+- Add desposit functionality
+- Add multiple accounts per user.
+- Add transfer functionality (between accounts)
+- Add transaction history
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account => ", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "anthony", "GG", "hunter")
+}
+
+func main() {
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if *seed {
+		fmt.Println("Seeding the database")
+		// Seed stuff
+		seedAccounts(store)
+	}
+
+	server := NewAPIServer(":3000", store)
+	server.Run()
+}
